f333d6ee07eef8794a2d35d8bfc0cc6f90b5654a
Thanh An Nguyen <Thanh An Nguyen>
2017-05-14 14:48:02 +0700

    cls

--- frontend/user-io/src/app/homesingle/content/newhome/newhouse.ts
+++
@@ -1,8 +0,0 @@
-export class NewHouse {
-    public title: String;
-    public description: String;
-    public address: String;
-    public price: Number;
-    public status: String;
-    public rate: String;
-}
