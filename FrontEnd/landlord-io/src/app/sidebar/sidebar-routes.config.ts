d1462b68488a33de386f10bd26407cf8587ad2e7
nghiant95 <trongnghia.msmart@gmail.com>
2017-05-15 16:22:16 +0700

    'Add-Valid'

--- FrontEnd/landlord-io/src/app/sidebar/sidebar-routes.config.ts
+++ FrontEnd/landlord-io/src/app/sidebar/sidebar-routes.config.ts
@@ -2,8 +2,8 @@ import { MenuType, RouteInfo } from './sidebar.metadata';
 export const ROUTES: RouteInfo[] = [
     { path: 'dashboard', title: 'Dashboard', menuType: MenuType.LEFT, icon: 'material-icons' },
     { path: './manageposts/newpost', title: 'New Post', menuType: MenuType.LEFT, icon: 'material-icons' },
-    { path: './manageposts/detailhouse', title: 'Detail House', menuType: MenuType.LEFT, icon: 'material-icons' },
-    { path: './infolandlord/profile', title: 'Landlord Profile', menuType: MenuType.LEFT, icon: 'material-icons' },
+    { path: './manageposts/detailhouse/1', title: 'Detail House', menuType: MenuType.LEFT, icon: 'material-icons' },
+    { path: './infolandlord/profile/1', title: 'Landlord Profile', menuType: MenuType.LEFT, icon: 'material-icons' },
     { path: './infolandlord/changepass', title: 'Change Password', menuType: MenuType.LEFT, icon: 'material-icons' },
     { path: './managerooms/createroomtype', title: 'Create Room Type', menuType: MenuType.LEFT, icon: 'material-icons' },
     { path: './managerooms/rooms', title: 'Rooms', menuType: MenuType.LEFT, icon: 'material-icons' }
