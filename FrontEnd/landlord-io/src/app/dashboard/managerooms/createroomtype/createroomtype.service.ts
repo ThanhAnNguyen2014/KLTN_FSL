3242fd8db0f0e2749b7631837d3a73ba6993277f
nghiant95 <trongnghia.msmart@gmail.com>
2017-06-15 02:27:41 +0700

    'complete-roomtype'

--- FrontEnd/landlord-io/src/app/dashboard/managerooms/createroomtype/createroomtype.service.ts
+++ FrontEnd/landlord-io/src/app/dashboard/managerooms/createroomtype/createroomtype.service.ts
@@ -6,34 +6,36 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class CreateroomtypeService {
 
-  private apiUrl = "http://5914085a08cca6001102777a.mockapi.io/RoomType/";
-  private apiUrlDevice = "http://5914085a08cca6001102777a.mockapi.io/Device/";
-  //private apiUrlDevice = "https://hcmutefslio.herokuapp.com/api/v1/devices/";
+  private apiUrl_all = "https://hcmutefslio.herokuapp.com/api/v1/roomtypes";
+  //private apiUrl = "http://5914085a08cca6001102777a.mockapi.io/RoomType/";
+  private apiUrl = "https://hcmutefslio.herokuapp.com/api/v1/roomtype/";
+  //private apiUrlDevice = "http://5914085a08cca6001102777a.mockapi.io/Device/";
+  private apiUrlDevice = "https://hcmutefslio.herokuapp.com/api/v1/devices/";
   constructor(
     private _http: Http
   ) { }
 
   Add(data: any): Observable<any> {
-    return this._http.post(this.apiUrl, data).map((response) => response.json());
+    return this._http.post(this.apiUrl, data).map((response) => response.json().result);
   }
 
   GetList(): Observable<any[]> {
-    return this._http.get(this.apiUrl).map((response) => response.json())
+    return this._http.get(this.apiUrl_all).map((response) => response.json().results)
   }
 
   Delete(id: object): Observable<any> {
-    return this._http.delete(this.apiUrl + id).map((response) => response.json())
+    return this._http.delete(this.apiUrl + id).map((response) => response.json().result)
   }
 
   GetSingle(id: object): Observable<any> {
-    return this._http.get(this.apiUrl + id).map((response) => response.json())
+    return this._http.get(this.apiUrl + id).map((response) => response.json().result)
   }
 
   Update(id: object, data: any): Observable<any> {
-    return this._http.put(this.apiUrl + id, data).map((response) => response.json())
+    return this._http.put(this.apiUrl + id, data).map((response) => response.json().result)
   }
 
   GetListDevice(): Observable<any[]> {
-    return this._http.get(this.apiUrlDevice).map((response) => response.json())
+    return this._http.get(this.apiUrlDevice).map((response) => response.json().results)
   }
 }
