8d2c5f48add46f402cd3ec61d4addc6a955ce590
ThanhAnNguyen2014 <kieuquanh94@gmail.com>
2017-06-17 16:11:31 +0700

    add email and password gmail

--- server/config.js
+++ server/config.js
@@ -1,8 +1,10 @@
-module.exports={
-   
-      secret_landlord:'fsl_io_landlords',
-      secret_user:'fsl_io_users',
+module.exports = {
+
+      secret_landlord: 'fsl_io_landlords',
+      secret_user: 'fsl_io_users',
       database_mLab: 'mongodb://thanhannguyen:Thanhan200114050@ds143211.mlab.com:43211/fsl_io',
-      database_local:'mongodb://localhost:27017/FSL_IO'
+      database_local: 'mongodb://localhost:27017/FSL_IO',
+      email: 'fslio.startup@gmail.com',
+      password: 'fslio@abc123',
 
 }
