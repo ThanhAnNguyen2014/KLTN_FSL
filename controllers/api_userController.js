50f6080f839e23e0d17ccf568ae0c4e4841954a4
ThanhAnNguyen2014 <kieuquanh94@gmail.com>
2017-06-18 04:21:41 +0700

    add login and register User

--- controllers/api_userController.js
+++ controllers/api_userController.js
@@ -3,7 +3,7 @@ var userServices = require('../services/user-service');
 var config = require('../server/config');
 var jwt = require('jsonwebtoken');
 var passport = require('passport');
-
+var nodemailer = require('nodemailer');
 var JwtStrategy = require('passport-jwt').Strategy,
     ExtractJwt = require('passport-jwt').ExtractJwt;
 
@@ -15,7 +15,7 @@ module.exports = {
             if (err) return res.status(500).json({
                 code: res.statusCode,
                 results: {
-                    message: 'err: ' + err,
+                    message: err,
                     doc: null
                 }
             });
@@ -40,35 +40,66 @@ module.exports = {
         });
     },
     /** create User  */
-    create: function (req, res, callback) {
-        userServices.create(req.body, function (err, doc) {
+    register: function (req, res, callback) {
+        var user = req.body;
+        userServices.create(user, function (err, doc) {
             if (err) return res.status(500).json({
                 code: res.statusCode,
                 results: {
-                    message: 'err: ' + err,
+                    message: err,
                     doc: null
                 }
             });
-            if (doc) {
-                console.log('Create User success!');
-                return res.status(200).json({
-                    code: res.statusCode,
-                    results: {
-                        message: null,
-                        doc: doc
-                    }
-                });
-            }
-        });
 
+            // create token
+            var token = 'JWT ' + jwt.sign({ id: user._id }, config.secret_user, {
+                expiresIn: '24h', // one day
+                algorithm: 'HS256'
+            });
+            // create reusable transporter object using the default SMTP transport
+            var transporter = nodemailer.createTransport({
+                service: 'gmail',
+                secure: true, // secure:true for port 465, secure:false for port 587
+                auth: {
+                    user: config.email,
+                    pass: config.password
+                }
+            });
+            // setup email data with unicode symbols
+            var mailOptions = {
+                from: '"FSL-IO 👻" <' + config.email + '>', // sender address
+                to: user.email, // list of receivers
+                subject: '[FSL-IO] Please verify your email address ✔.', // Subject line
+                text: 'Verify Account from system FSL-IO', // plain text body
+                html: '<a href="' + config.urlVerifyAccount_User + token + '">Click to verify account</a>', // html body
+                html: '<p>Hi <strong>@' + user.firstname + '!</strong></p><p>Help us secure your FSL-IO &nbsp;account by verifying your email address (' + user.email + '). This lets you access all of FSL-IO' + "'s" + ' features.</p><p><a href="' + config.urlVerifyAccount_User + token + '">Verify email adderss</a></p><p>Button not working? Please the following link into your browser:</p><p><a href="' + config.urlVerifyAccount_User + token + '">' + config.urlVerifyAccount_User + token + '</a></p><p>You' + "'" + 're receiving this email because you recently create a new FSL-IO &nbsp;account or added a new email address. If this wasn' + "'" + 't you, please ignore this email.</p>'
+            };
+            // send mail with defined transport object
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    return console.log(error);
+                }
+                console.log('Message %s sent: %s', info.messageId, info.response);
+            });
+            return res.status(200).json({
+                code: res.statusCode,
+                results: {
+                    message: 'You have successfully registered. You need to verify the email to use the account',
+                    doc: null,
+                    url: config.urlVerifyAccount_User + token
+                }
+            });
+        });
     },
     /**update User */
     updateUserById: function (req, res, callback) {
-        userServices.update(req.params.id, req.body, function (err, doc) {
+        var id = req.params.id;
+        var user = req.body;
+        userServices.update(id, user, function (err, doc) {
             if (err) return res.status(500).json({
                 code: res.statusCode,
                 results: {
-                    message: 'err: ' + err,
+                    message: err,
                     doc: null
                 }
             });
@@ -93,11 +124,12 @@ module.exports = {
         });
     },
     deleteUser: function (req, res, callback) {
-        userServices.remove(req.params.id, function (err, doc) {
+        var id = req.params.id;
+        userServices.remove(id, function (err, doc) {
             if (err) return res.status(500).json({
                 code: res.statusCode,
                 results: {
-                    message: 'err: ' + err,
+                    message: err,
                     doc: null
                 }
             });
@@ -105,8 +137,8 @@ module.exports = {
                 return res.status(200).json({
                     code: res.statusCode,
                     results: {
-                        message: null,
-                        doc: doc
+                        message: 'Deleted object success!',
+                        doc: null
                     }
                 });
             }
@@ -120,14 +152,13 @@ module.exports = {
                 });
             }
         });
-        return res.status(500);
     },
     getAllUser: function (req, res, callback) {
         userServices.findAll(function (err, docs) {
             if (err) return res.status(500).json({
                 code: res.statusCode,
                 results: {
-                    message: 'err: ' + err,
+                    message: err,
                     doc: null
                 }
             });
@@ -159,7 +190,8 @@ module.exports = {
             return res.status(500).json({
                 code: res.statusCode,
                 results: {
-                    message: 'err: Username or Password not empty!'
+                    message: 'Username or Password not empty!',
+                    doc: null
                 }
             });
         }
@@ -168,7 +200,7 @@ module.exports = {
                 if (err) res.status(500).json({
                     code: res.statusCode,
                     results: {
-                        message: 'err: ' + err,
+                        message: err,
                         doc: null
                     }
                 });
@@ -177,7 +209,7 @@ module.exports = {
                         code: res.statusCode,
                         results: {
                             success: false,
-                            message: 'User not found!',
+                            message: 'Username or email not registered',
                             doc: null
                         }
                     });
@@ -186,28 +218,40 @@ module.exports = {
                     if (err) res.status(500).json({
                         code: res.statusCode,
                         results: {
-                            message: 'err: ' + err,
+                            message: err,
                             doc: null
                         }
                     });
                     if (isMatch) {
-                        var token = jwt.sign({ id: user.id }, config.secret_user, {
-                            expiresIn: '1h', // one house
-                            algorithm: 'HS256'
-                        });
-                        res.status(200).json({
-                            code: res.statusCode,
-                            results: {
-                                message: null,
-                                doc: {
-                                    success: true,
-                                    token: 'JWT ' + token,
+                        // check landlord active
+                        if (user.active) {
+                            var token = jwt.sign({ id: user.id }, config.secret_user, {
+                                expiresIn: '24h', // one house
+                                algorithm: 'HS256'
+                            });
+                            res.status(200).json({
+                                code: res.statusCode,
+                                results: {
+                                    message: null,
+                                    doc: {
+                                        success: true,
+                                        token: 'JWT ' + token,
+                                    }
                                 }
-                            }
-                        });
+                            });
+                        }
+                        else {
+                            res.status(401).json({
+                                code: res.statusCode,
+                                results: {
+                                    message: 'You have not yet activated your account, please email to activate your account',
+                                    doc: null
+                                }
+                            });
+
+                        }
                     }
                     else {
-                        console.log(user.password);
                         return res.status(200).json({
                             code: res.statusCode,
                             results: {
@@ -221,7 +265,6 @@ module.exports = {
                 });
             });
         }
-
     },
     /**Change Password */
     changPassword: function (req, res) {
@@ -241,7 +284,7 @@ module.exports = {
             if (err) res.status(500).json({
                 code: res.statusCode,
                 results: {
-                    message: 'err: ' + err,
+                    message: err,
                     doc: null
                 }
             });
@@ -250,7 +293,7 @@ module.exports = {
                     if (err) res.status(500).json({
                         code: res.statusCode,
                         results: {
-                            message: 'err: ' + err,
+                            message: err,
                             doc: null
                         }
                     });
@@ -294,7 +337,94 @@ module.exports = {
                 });
             }
         });
-    }
+    },
+    validates: function (req, res) {
+        var username = req.body.username;
+        var email = req.body.email;
+        var password = req.body.password;
+        var passwordconfirm = req.body.passwordconfirm;
+
+        userServices.validate(username, email, password, passwordconfirm, (err, result) => {
+            if (err) {
+                return res.status(500).json({
+                    code: res.statusCode,
+                    results: {
+                        message: err,
+                        doc: null
+                    }
+                });
+            }
+            return res.status(200).json({
+                code: res.statusCode,
+                results: {
+                    message: result,
+                    doc: null
+                }
+            });
+        });
+    },
+    verifyEmail: function (req, res) {
+        // check header or url parameters or post parameters for token
+        const token = req.body.token || req.query.token || req.headers['x-access-token'];
+        // decode token 
+        jwt.verify(token.split(' ')[1], config.secret_user, function (err, decode) {
+            if (err) {
+                req.userId = undefined;
+                return res.status(500).json({
+                    code: res.statusCode,
+                    results: {
+                        message: err,
+                        doc: null
+                    }
+                });
+            }
+            else {
+                req.userId = decode;
+                userServices.getUserById(req.userId.id, (err, doc) => {
+                    if (err) {
+                        return res.status(500).json({
+                            code: res.statusCode,
+                            results: {
+                                message: err,
+                                doc: null
+                            }
+                        });
+                    }
+                    if (doc) {
+                        doc.tokenjwt = token;
+                        doc.active = true;
+                        userServices.update(req.userId.id, doc, (err, result) => {
+                            if (err) {
+                                return res.status(500).json({
+                                    code: res.statusCode,
+                                    results: {
+                                        message: err,
+                                        doc: null
+                                    }
+                                });
+                            }
+                            return res.status(200).json({
+                                code: res.statusCode,
+                                results: {
+                                    message: 'Your account is now verified. Congratulations!',
+                                    doc: result
+                                }
+                            });
+                        })
+                    }
+                    else {
+                        return res.status(404).json({
+                            code: res.statusCode,
+                            results: {
+                                message: 'Landlord not found!',
+                                doc: null
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    },
 
 }
 var opts = {};
